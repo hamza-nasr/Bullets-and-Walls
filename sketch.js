ae366ac2ae46821942dd590e2c4df355fcab2272
hamza-nasr <68786227+hamza-nasr@users.noreply.github.com>
2020-09-02 16:04:35 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var bullet, wall, speed, weight, thickness;
 function setup() {
   createCanvas(1600,400);
   bullet = createSprite(50, 200, 50, 50);
-  wall = createSprite(400,200,thickness,200);
+  wall = createSprite(1200,200,thickness,200);
   wall.shapeColor = color(80,80,80);
   speed=random(223,321);
   weight=random(30,52);
@@ -14,21 +14,29 @@ function setup() {
 
 function draw() {
   background("black");  
-
-  if(wall.x = bullet.x<(wall.width + bullet.width)/2){
-    bullet.velocityX = 0;
-    deformation =0.5*weight*speed*speed/22500;
-    if(deformation<180){
-      bullet.shapeColor = (255,0,0);
-    } 
-    if(deformation<180 && deformation>100 ){
-      bullet.shapeColor = (230,230,0);
-    }   
-    if(deformation<100){
-      bullet.shapeColor = (0,255,0);
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX = 0
+    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+    if(damage > 10){
+      bullet.shapeColor =color(255,0,0);
+    }
+    if(damage < 10){
+      bullet.shapeColor =color(0,255,0);
     }
   }
+
+  
+  
   drawSprites();
 }
+function hasCollided(lbullet, lwall){
+  bulletRightEdge = lbullet.x + lbullet.width;
+  wallLeftEdge = lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
-//why is the wall on the right
